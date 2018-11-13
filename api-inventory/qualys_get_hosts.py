ab8c8e8a35885b3a50d02df2c68425fc124671ae
kamaral75 <kamaral@broadinstitute.org>
2018-11-13 16:19:06 -0500

    Basic XML parsing works!

--- api-inventory/qualys_get_hosts.py
+++ api-inventory/qualys_get_hosts.py
@@ -5,6 +5,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 import logging
+import xml.etree.ElementTree as ET
 
 class QualysAPIInventory(object):
 
@@ -23,8 +24,32 @@ class QualysAPIInventory(object):
     headers={'X-Requested-With':'Python'}
 
     app_get_response = self.getHTTP(url=url_get_hosts, params='', headers=headers, auth=auth)
-    print app_get_response.content
-    print(app_get_response)
+
+    # Convert XML string Element to ElementTree
+    response_xml = ET.ElementTree(ET.fromstring(app_get_response.content))
+    # Get XML root element
+    root = response_xml.getroot()
+
+    # Traverse XML response structure to get HOST_LIST
+    res_host_list = root[0][1]
+    res_date_time = root[0][0]
+
+    # Initialize empty list to store hosts
+    host_list = []
+
+    count = 0
+    # Iterate each host in HOST_LIST of response XML data
+    for hosts in res_host_list:
+      # Initialize empty dictionary to store a single host row
+      host_row = {}
+      for host in hosts.iter():
+        # Iterate each data value in each host
+        if not host.tag == 'HOST':
+          # Append each key value pair to to host row dictionary
+          # host.tag is the XML element name and host.text is the XML element value
+          host_row[host.tag] = host.text
+      # Add each host to list of hosts
+      print(host_row)
 
 
   """ Configure requests module to retry session connection """
